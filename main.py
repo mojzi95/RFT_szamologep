6b130be5c22b2be2ff5f70dc51a7432b0f30eba2
mojzi95 <toth.tamas19950628@gmail.com>
2022-10-23 19:58:19 +0200

    setup basic window for project

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+root=Tk();
+root.title("Számológép");
+root.geometry("570x600+100+200");
+root.resizable(False, False);
+root.config(bg="#000000");
+
+
+root.mainloop();
